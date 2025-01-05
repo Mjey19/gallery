87f05fe624b88b2be161f8e26f4d03856cdf49b9
Jack Cezar <jackcezar@mail.ru>
2025-01-05 17:32:44 +0300

    update card

--- src/entities/catalog/card/ui/card.tsx
+++ src/entities/catalog/card/ui/card.tsx
@@ -4,12 +4,12 @@ import { memo } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 export const Card = memo(function ({ src, photographer, name }: CatalogTypes) {
   return (
-    <div className="min-h-[350px] w-1/3 flex flex-col justify-between rounded-t-[5px] cursor-pointer bg-white mb-4 break-inside-avoid overflow-x-hidden shadow-lg">
+    <div className="min-h-[300px] w-1/3 flex flex-col justify-between rounded-t-[5px] cursor-pointer bg-white mb-4 break-inside-avoid overflow-x-hidden shadow-lg">
       <CardContent className="w-full max-h-72 overflow-y-hidden">
         {/* <div className="object-cover w-full max-h-72 min-h-52"></div> */}
         <LazyLoadImage
           width="100%"
-          height={288}
+          height="100%"
           src={src?.landscape}
           // visibleByDefault={!!src?.original}
           loading="lazy"
